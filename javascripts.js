46565886737b9e17d7c05a159c4fd2d223dca789
J3pp3s <jespher2010@live.com>
2021-11-11 19:12:48 +0100

    first commit

---
+++ javascripts.js
@@ -0,0 +1,5 @@
+// Skapar en funktion för submitForm på kontaktsidan. 
+// Skickar ett alert-meddelande när required-formulären har validerats.
+function submitForm(){
+    alert("Skickar mail!");
+}
